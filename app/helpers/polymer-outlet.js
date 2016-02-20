2b3ce7f2d5d2a419d0dd4e6619da2342510f8b39
Marius Herget <info@herget.design>
2016-02-20 23:38:14 +0100

    before polymer-outlet

---
+++ app/helpers/polymer-outlet.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function polymerOutlet(params/*, hash*/) {
+  return params;
+}
+
+export default Ember.Helper.helper(polymerOutlet);
---
+++ tests/unit/helpers/polymer-outlet-test.js
@@ -0,0 +1,10 @@
+import { polymerOutlet } from 'carpe-diem/helpers/polymer-outlet';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | polymer outlet');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = polymerOutlet([42]);
+  assert.ok(result);
+});
