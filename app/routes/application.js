728099b693aa1ad229d56898c625c3885707b223
kingmarv <kingmarv@online.de>
2016-02-21 12:38:32 +0100

    Added some test files

--- app/routes/application.js
+++ app/routes/application.js
@@ -7,15 +7,33 @@ export default Ember.Route.extend({
         }
     },
     model() {
+        let that = this;
         this.store.createRecord('user', {
             id: 'kingmarv',
             email: 'marvin@mail.ru'
         });
-        this.store.createRecord('day', {
-            id: 20160220,
+        let day = this.store.createRecord('day', {
+            //id: 20160220,
             owner: this.store.peekRecord('user', 'kingmarv'),
             date: new Date('2016-02-20'),
             checksum: '2'
+        });
+        day.save().then(function(day) {
+            console.log(day.get('id'));
+            let event = that.store.createRecord('event', {
+                //id: 2,
+                type: 1,
+                priority: 0,
+                title: 'An event',
+                start: new Date('2016-02-20T15:00'),
+                end: new Date('2016-02-20T16:00'),
+                //duration: 3600000,
+                description: 'A description',
+                location: null,
+                suggestion: 0,
+                day: day
+            });
+            event.save();
         });
         this.store.createRecord('day', {
             id: 20160221,
@@ -23,20 +41,6 @@ export default Ember.Route.extend({
             date: new Date('2016-02-20'),
             checksum: '2'
         });
-        let event = this.store.createRecord('event', {
-            //id: 2,
-            type: 1,
-            priority: 0,
-            title: 'An event',
-            start: new Date('2016-02-20T15:00'),
-            end: new Date('2016-02-20T16:00'),
-            //duration: 3600000,
-            description: 'A description',
-            location: null,
-            suggestion: 0,
-            //day: this.store.peekRecord('day', 20160220)
-        });
-        event.save();
         this.store.createRecord('event', {
             id: 1,
             type: 2,
