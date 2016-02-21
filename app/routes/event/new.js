bae00cf50bc5663669e5727e2253b20ca23cbc1e
kingmarv <kingmarv@online.de>
2016-02-21 13:03:47 +0100

    Added model to routing

--- app/routes/event/new.js
+++ app/routes/event/new.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(params) {
+        let locations = this.store.findAll('location');
+        let events = this.store.findAll('event');
+        return Ember.RVSP.hash({
+            locations: locations,
+            events: events
+        })
+    }
 });
