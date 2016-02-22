46b99a0fe5782c6bdbc565bc0fd47397338fc9d3
kingmarv <kingmarv@online.de>
2016-02-22 07:09:48 +0100

    Added event creation functionality

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-    host: 'http://192.168.0.16:3000',
+    host: 'http://169.53.137.142:3000',
     namespace: 'api',
     headers: {
         'Authorization': 'Bearer ' + Cookies.get('token')
--- app/routes/event/new.js
+++ app/routes/event/new.js
@@ -21,9 +21,27 @@ export default Ember.Route.extend({
     },
     actions: {
       googleAutocomplete(){
-        console.log('k.,mf');
         initAutocomplete();
         return true;
+      },
+      saveEvent(name, selLati, selLongi, eveTitle, eveDescr, eveStart, eveEnd, eveDur) {
+          let that = this;
+          let location = this.store.createRecord('location', {
+              name: name,
+              latitude: selLati,
+              longitude: selLongi
+          });
+          location.save().then(function(location) {
+              let event = that.store.createRecord('event', {
+                  type: parseInt(Ember.$('#eveType paper-radio-button[aria-checked=true]').attr('name')),
+                  title: eveTitle,
+                  description: eveDescr,
+                  start: new Date(eveStart),
+                  end: new Date(eveEnd),
+                  location: location
+              });
+              event.save();
+          });
       }
     }
 });
